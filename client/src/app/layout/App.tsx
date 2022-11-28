e86dbb1d35958bf31a7cfbf97a58fbb57f442bec
doanth2 <doanthieuchinh@gmail.com>
2022-11-28 14:50:03 +0900

    add router dom

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,30 +1,41 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import Catalog from "../../features/catalog/Catalog";
+import { Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import { useState } from 'react';
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
+import Catalog from "../../features/catalog/Catalog";
+
 function App() {
     const [darkMode, setDarkMode] = useState(false)
     const paletteType = darkMode ? 'dark' : 'light';
     const theme = createTheme({
         palette: {
             mode: paletteType,
-            background:{
-                default:paletteType ==='light'? '#eaeaea' : '#121212'
+            background: {
+                default: paletteType === 'light' ? '#eaeaea' : '#121212'
             }
         }
     })
     const handleThemeChange = () => {
         setDarkMode(!darkMode)
-      
+
     }
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Header  darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+            <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
             <Container>
-                <Catalog />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/catalog/:id" element={<ProductDetails />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                </Routes>
             </Container>
-
         </ThemeProvider>
     )
 }
